83b34561937e943e4f4ec89617b240a9d78662b0
Tiancheng Mai <tiancheng.mai@sydney.edu.au>
2022-10-09 01:38:21 +1100

    add: init files

---
+++ eavesdropper.py
@@ -0,0 +1,12 @@
+import os
+import socket
+import sys
+
+
+def main():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,17 @@
+import os
+import socket
+import sys
+
+
+# Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
+PERSONAL_ID = ''
+PERSONAL_SECRET = ''
+
+
+def main():
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    main()
