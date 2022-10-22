377d8fde65a4d4a6e0e3141aadbabd9e28308656
Yangqing Zheng <yzhe6302@uni.sydney.edu.au>
2022-10-22 16:40:09 +1100

    personal id

--- client.py
+++ client.py
@@ -10,8 +10,14 @@ PERSONAL_SECRET = '113619c855557bbe68464878e6aea7d3'
 
 def main():
     # TODO
-    pass
+    try:
+        conf_path = open(sys.argv[1],'r')
+    except FileNotFoundError:
+        sys.exit(1)
+    print(conf_path.read())
+    
 
+    
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -4,13 +4,18 @@ import sys
 
 
 # Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = ''
-PERSONAL_SECRET = ''
+PERSONAL_ID = 'B03FFA'
+PERSONAL_SECRET = '113619c855557bbe68464878e6aea7d3'
 
 
 def main():
     # TODO
     pass
+
+
+            
+    
+    
 
 
 if __name__ == '__main__':
