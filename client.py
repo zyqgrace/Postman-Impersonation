1930aa25563a6bc7c645f5771d964a8988274cdc
Yangqing Zheng <yzhe6302@uni.sydney.edu.au>
2022-10-22 16:23:04 +1100

    personal id

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ import sys
 
 
 # Visit https://edstem.org/au/courses/8961/lessons/26522/slides/196175 to get
-PERSONAL_ID = ''
-PERSONAL_SECRET = ''
+PERSONAL_ID = 'B03FFA'
+PERSONAL_SECRET = '113619c855557bbe68464878e6aea7d3'
 
 
 def main():
