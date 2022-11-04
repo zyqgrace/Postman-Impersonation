cb417c0cf0c17e4ba9a7b532de2ab0ed4e8a923b
Yangqing Zheng <yzhe6302@uni.sydney.edu.au>
2022-11-04 11:42:30 +1100

    ear_test

--- eavesdropper.py
+++ eavesdropper.py
@@ -36,29 +36,28 @@ def main():
     # TODO
     IP = "localhost"
     server_port, client_port, path = parse_conf_path()
-    print("client_port: "+client_port,flush=True)
     MAIl_FROM = None
     RCPT_to = []
     text = ''
     filename = None
     try:
-        pretent_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        pretent_client.connect((IP,server_port))
+        AS = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        AS.connect((IP,server_port))
     except Exception:
         print("AS: Cannot establish connection",end="\r\n",flush=True)
         sys.exit(3)
 
     try:
-        pretent_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        pretent_server.bind((IP,client_port))
-        pretent_server.listen()
-        conn, addr = pretent_server.accept()
+        AC = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        AC.bind((IP,client_port))
+        AC.listen()
+        conn, addr = AC.accept()
     except Exception:
         print("AC: Connection lost",end="\r\n",flush=True)
         sys.exit(3)
 
     while True:
-        recved = pretent_client.recv(1024)
+        recved = AS.recv(1024)
         info = recved.decode()
         if not recved:
             print("AS: Connection lost",end="\r\n",flush=True)
@@ -74,7 +73,7 @@ def main():
             sys.exit(3)
         print("C: "+info.strip("\r\n"),end="\r\n",flush=True)
         print("AS: "+info.strip("\r\n"),end="\r\n",flush=True)
-        pretent_client.send(recved)
+        AS.send(recved)
         
 if __name__ == '__main__':
     main()
