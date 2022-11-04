ee783006717942a0fb8385cdccc90998c7ad7903
Yangqing Zheng <yzhe6302@uni.sydney.edu.au>
2022-11-04 23:06:20 +1100

    prefix_stdout

--- client.py
+++ client.py
@@ -14,11 +14,15 @@ class Email:
         self.subject = subject
         self.body = body
 
-def parse_conf_path():
-    server_port = None
-    send_path = None
+def parse_conf():
+    '''
+    parse the configuration file
+    return the server_port number and absolution path of send_path
+    '''
     if len(sys.argv)<= 1:
         sys.exit(1)
+    server_port = None
+    send_path = None
     try:
         conf_path = open(sys.argv[1],'r')
         configuration = conf_path.readlines()
@@ -30,19 +34,16 @@ def parse_conf_path():
                 send_path = info[10:]
     except FileNotFoundError:
         sys.exit(1)
-    if server_port.isnumeric() == False:
+    if server_port == None or send_path == None:
         sys.exit(2)
     elif int(server_port) <= 1024:
-        sys.exit(2)
-    elif send_path == None:
         sys.exit(2)
     send_path = os.path.expanduser(send_path)
     return int(server_port),send_path
 
-def directory_lis(send_path):
+def list_directory(send_path):
     """
-    parameter: the send path after parsing example(~/send)
-    This will return all the files in alphabetically order
+    return all the files in the send_path in alphabetically order
     """
     files = []
     if(not os.path.isdir(send_path)):
@@ -58,34 +59,32 @@ def directory_lis(send_path):
 def read_text(filepath):
     """
     This aims to read the text from absolute filepath parse
-    and return information as an Email instance
+    and return send status and information as an Email instance
     """
-    try:
-        f = open(filepath,"r")
-        texts = f.readlines()
-        From = None
-        To = None
-        Date = None
-        Subject = None
-        Body = []
-        if texts[0][0:4] == "From":
-            From = texts[0][6:-1]
-        if texts[1][0:2] == "To":
-            To = texts[1][4:-1]
-        if texts[2][0:4] == "Date":
-            Date = texts[2][0:-1]
-        if texts[3][0:7] == "Subject":
-            Subject = texts[3][0:-1]
-        i = 4
-        while i < len(texts):
-            Body.append(texts[i].strip("\n"))
-            i+=1
-        if From == None or To == None or Date == None or Subject == None:
-            print("C: "+os.path.abspath(filepath)+": Bad formation",end="\r\n",flush=True)
-            sys.exit(0)
-    except Exception:
-        print("error")
-    return Email(From, To, Date, Subject, Body)
+    f = open(filepath,"r")
+    texts = f.readlines()
+    send_status = True
+    From = None
+    To = None
+    Date = None
+    Subject = None
+    Body = []
+    if texts[0][0:4] == "From":
+        From = texts[0][6:-1]
+    if texts[1][0:2] == "To":
+        To = texts[1][4:-1]
+    if texts[2][0:4] == "Date":
+        Date = texts[2][0:-1]
+    if texts[3][0:7] == "Subject":
+        Subject = texts[3][0:-1]
+    i = 4
+    while i < len(texts):
+        Body.append(texts[i].strip("\n"))
+        i+=1
+    if From == None or To == None or Date == None or Subject == None:
+        print("C: "+os.path.abspath(filepath)+": Bad formation",end="\r\n",flush=True)
+        send_status = False
+    return send_status, Email(From, To, Date, Subject, Body)
 
 
 def send_email_via_server(client_socket, email):
@@ -149,17 +148,18 @@ def check_status_code(client_sock: socket.socket, status_code: int) -> None:
 def main():
     # TODO
     IP = 'localhost'
-    PORT, send_path = parse_conf_path()
-    files = directory_lis(send_path)
-    for filepath in files:
-        dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        email = read_text(filepath)
-        try:
-            dataSocket.connect((IP,PORT))
-        except ConnectionRefusedError:
-            print("C: Cannot establish connection",end="\r\n",flush=True) 
-            sys.exit(3)
-        send_email_via_server(dataSocket,email)
+    PORT, send_path = parse_conf()
+    file_paths = list_directory(send_path)
+    dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        dataSocket.connect((IP,PORT))
+    except ConnectionRefusedError:
+        print("C: Cannot establish connection",end="\r\n",flush=True) 
+        sys.exit(3)
+    for filepath in file_paths:
+        send_status, email = read_text(filepath)
+        if send_status:
+            send_email_via_server(dataSocket,email)
 
 if __name__ == '__main__':
     main()
